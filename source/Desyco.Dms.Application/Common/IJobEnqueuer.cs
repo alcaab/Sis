2182c958595d93df591d9145c6c89533096a3b5c
alcaab <alcaab@gmail.com>
2025-11-28 19:45:19 -0400

    incluir el archivo de programa a git

--- source/Desyco.Dms.Application/Common/IJobEnqueuer.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Linq.Expressions;
-
-namespace Desyco.Dms.Application.Common;
-
-public interface IJobEnqueuer
-{
-    string EnqueueBackgroundJob<T>(Expression<Func<T, Task>> methodCall);
-}
