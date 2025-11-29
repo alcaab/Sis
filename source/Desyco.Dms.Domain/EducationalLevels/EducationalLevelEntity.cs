7a9ee2cd4feac544eb8878d0a00d5f0fca67b5e1
alcaab <alcaab@gmail.com>
2025-11-29 09:16:39 -0400

    EducationalLevelEntity

--- source/Desyco.Dms.Domain/EducationalLevels/EducationalLevelEntity.cs
+++ source/Desyco.Dms.Domain/EducationalLevels/EducationalLevelEntity.cs
@@ -2,5 +2,9 @@ namespace Desyco.Dms.Domain.EducationalLevels;
 
 public class EducationalLevelEntity : EntityBase<int>
 {
+    public EducationalLevelType LevelTypeId { get; set; }
+    
     public string Name { get; set; } = string.Empty;
+
+    public bool IsActive { get; set; }
 }
